c3738ecdbdbcfacbfe015ab57a2e75046ca021ff
rubyhpsek <sensorbrain@gmail.com>
2014-11-29 01:18:58 +0000

    update config for heroku

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -20,6 +20,9 @@ Spree.config do |config|
    # config.attachment_url = ":s3_eu_url"
    # config.s3_host_alias = "s3-eu-west-1.amazonaws.com"
 end
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
 end
 
 Spree.user_class = "Spree::User"
