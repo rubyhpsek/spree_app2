dbb57623d2b9bb3fc6a2e06824ab2bf139768f5b
rubyhpsek <sensorbrain@gmail.com>
2014-12-05 10:43:09 +0000

    try fix upload of images

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require "aws/s3"
+AWS::S3::DEFAULT_HOST.replace "s3-eu-west-1.amazonaws.com"
