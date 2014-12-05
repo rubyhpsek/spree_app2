1ce711c8901fc8f1acfd3de0e1ebbf052216db19
rubyhpsek <sensorbrain@gmail.com>
2014-12-05 02:44:31 +0000

    try configure aws S3

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,7 +10,38 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
    # config.admin_interface_logo = 'logo/spree_50.png.png'
-   config.logo = 'store_logo1.png'
+   config.logo = 'store_logo2.png'
+
+
+   attachment_config = {
+
+  s3_credentials: {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    bucket:            ENV['S3_BUCKET_NAME']
+  },
+
+  storage:        :s3,
+  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+  s3_protocol:    "https",
+  bucket:         ENV['S3_BUCKET_NAME'],
+  url:            ":s3_domain_url",
+
+  styles: {
+      mini:     "48x48>",
+      small:    "100x100>",
+      product:  "240x240>",
+      large:    "600x600>"
+  },
+
+  path:           "/spree/:class/:id/:style/:basename.:extension",
+  default_url:    "/spree/:class/:id/:style/:basename.:extension",
+  default_style:  "product"
+}
+
+attachment_config.each do |key, value|
+  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+end
 
 
 =begin
@@ -20,8 +51,8 @@ Spree.config do |config|
    config.s3_secret = ""
 =end
    # if you create your Amazon S3 bucket on Western Europe server, you need these two additional options:
-   # config.attachment_url = ":s3_eu_url"
-   # config.s3_host_alias = "s3-eu-west-1.amazonaws.com"
+    config.attachment_url = ":s3_eu_url"
+   config.s3_host_alias = "s3-eu-west-1.amazonaws.com"
 end
 
 =begin
