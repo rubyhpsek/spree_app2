16174ff0317663adc64e254f1d9203c4e0f2b2e9
rubyhpsek <sensorbrain@gmail.com>
2014-12-05 11:43:05 +0000

    deploy

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+# require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Rails.application.initialize!
+# Rails.application.initialize!
 
  require "aws/s3"
  AWS::S3::DEFAULT_HOST.replace "s3-eu-west-1.amazonaws.com"
