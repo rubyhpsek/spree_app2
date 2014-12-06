42eb5b2ac3b43d03f1b85404a737d9247126ad29
rubyhpsek <sensorbrain@gmail.com>
2014-12-06 00:30:04 +0000

    modification

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,9 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+ Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+        "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+
+
+          end
