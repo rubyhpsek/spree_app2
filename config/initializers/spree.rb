96fa1d53a91bb360f5ff1ff8dac591833b3ed6a1
rubyhpsek <sensorbrain@gmail.com>
2014-12-06 11:44:06 +0000

    one

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -72,9 +72,9 @@ end
 
 
 
- Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+          Paperclip.interpolates(:s3_eu_url) do |attachment, style|
         "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-        end
+end
 
 
 
