9f827963e73ced6e9795aeb9a9a2a1aaffd2d088
rubyhpsek <sensorbrain@gmail.com>
2014-12-07 14:53:46 +0000

    change some files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
 
    # Required for Devise. Remember to change localhost:3000 to actual application host
-  config.action_mailer.default_url_options = { :PORT => '5000' }
+  config.action_mailer.default_url_options = { :HOST => 'https://spreeshop2014.herokuapp.com/' }
 
 
   
