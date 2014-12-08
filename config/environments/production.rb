fb255f8c5a1f7e90b98427fcd4e2b84df06e8498
rubyhpsek <sensorbrain@gmail.com>
2014-12-08 01:33:07 +0000

    try redeploy go johnny googoo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,10 @@ Rails.application.configure do
           },
           :path => ":class/:id/:basename_:style.:extension",
           :url => "s3-eu-west-1.amazonaws.com"
-      }
+          # config/initializers/paperclip.rb
 
+      }
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 end
